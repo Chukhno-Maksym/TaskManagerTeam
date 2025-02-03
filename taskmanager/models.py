05d4655c7d447d5f73ac4a1c73a0ff78000a78dd
Maksym <chukhno.maksym@gmail.com>
2025-02-03 02:14:57 +0200

    Starting project and change project settings. Creating models and make migrations.

---
+++ taskmanager/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+PRIORITY_CHOICES = [
+    ("low", "Low"),
+    ("medium", "Medium"),
+    ("high", "High"),
+]
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey("Position", on_delete=models.PROTECT)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="workers")
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default="medium",
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+
+
