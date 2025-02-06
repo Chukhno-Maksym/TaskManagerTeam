845a0edb3e1c5350c988f80966f4669048166c9e
Maksym <chukhno.maksym@gmail.com>
2025-02-06 11:47:34 +0200

    Changes

--- teamtaskmanager/settings/base.py
+++ teamtaskmanager/settings/base.py
@@ -14,19 +14,11 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h%@mc+5=mxy%8mfox(bt7h8_qtaa2cntdf^%4oxu8-obwx_r_z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -70,17 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'teamtaskmanager.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
